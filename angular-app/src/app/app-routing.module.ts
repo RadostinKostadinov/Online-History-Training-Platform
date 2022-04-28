2bf4fed2662c020aa752356f641622ec13e6439d
Radostin Kostadinov <bg.radostin.kostadinov@gmail.com>
2022-04-28 19:51:51 +0300

    Feat: add profile routing
    Now "Profile" button in header redirects to Profile page,
    also add routing for side menu in profile page

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,3 +1,9 @@
+import { AvatarsCheckComponent } from './components/profile/avatars-check/avatars-check.component';
+import { CompetitionsComponent } from './components/profile/competitions/competitions.component';
+import { EditResourcesComponent } from './components/profile/edit-resources/edit-resources.component';
+import { ViewProfilesComponent } from './components/profile/view-profiles/view-profiles.component';
+import { EditProfilesComponent } from './components/profile/edit-profiles/edit-profiles.component';
+import { InfoComponent } from './components/profile/info/info.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LoggedInGuard } from './components/auth/logged-in-guard';
 import { AuthGuard } from './components/auth/auth-guard';
@@ -9,10 +15,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent, canActivate: [LoggedInGuard]},
-  { path: 'register', component: RegisterComponent, canActivate: [LoggedInGuard]},
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
-  { path: '**', redirectTo: ''}
+  { path: 'login', component: LoginComponent, canActivate: [LoggedInGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [LoggedInGuard] },
+  {
+    path: 'profile', component: ProfileComponent, canActivate: [AuthGuard], children: [
+      { path: '', pathMatch: 'full', redirectTo: 'info' },
+      { path: 'info', component: InfoComponent, canActivate: [AuthGuard] },
+      { path: 'view-profiles', component: ViewProfilesComponent, canActivate: [AuthGuard] },
+      { path: 'edit-profiles', component: EditProfilesComponent, canActivate: [AuthGuard] },
+      { path: 'edit-resources', component: EditResourcesComponent, canActivate: [AuthGuard] },
+      { path: 'competitions', component: CompetitionsComponent, canActivate: [AuthGuard] },
+      { path: 'avatars-check', component: AvatarsCheckComponent, canActivate: [AuthGuard] }
+    ]
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
