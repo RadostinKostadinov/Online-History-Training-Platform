3033f905c679ce654d52ee5288502b85ea940bc5
Radostin Kostadinov <bg.radostin.kostadinov@gmail.com>
2022-04-29 11:37:57 +0300

    S:Feat: add routes for register requests
     * Added route "users/get/register-requests", which returns all
    users that aren't approved yet (isApproved: false).
     * Added route "users/post/register-requests/:profileId", which
    changes the status of user with id == profileId
    to approved (isApproved: true)

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,14 +6,60 @@ const {
     validateTeacher,
 } = require("../middlewares/verifyToken.js");
 
-//Връща профил с дадено ID
-router.get("/get/:profileId", validateUser, validateTeacher, async (req, res) => {
-    try {
-        const user = await User.findById(req.params.profileId);
-        res.status(200).json(user);
-    } catch (err) {
-        res.status(500).json(err);
+//Връща всички профили, чакащи потвърждение за регистрация
+router.get(
+    "/get/register-requests",
+    validateUser,
+    validateTeacher,
+    async (req, res) => {
+        try {
+            const users = await User.find({ isApproved: false }).select({
+                _id: 1,
+                firstName: 1,
+                surName: 1,
+                lastName: 1,
+                class: 1,
+                number: 1,
+            });
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
-});
+);
+
+//Одобрява профил с дадено ID
+router.post(
+    "/post/register-requests/:profileId",
+    validateUser,
+    validateTeacher,
+    async (req, res) => {
+        try {
+            const approvedUser = await User.findByIdAndUpdate(
+                req.params.profileId,
+                { isApproved: true },
+                { new: true }
+            );
+            res.status(200).json(approvedUser);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+);
+
+//Връща профил с дадено ID
+router.get(
+    "/get/:profileId",
+    validateUser,
+    validateTeacher,
+    async (req, res) => {
+        try {
+            const user = await User.findById(req.params.profileId);
+            res.status(200).json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+);
 
 module.exports = router;
